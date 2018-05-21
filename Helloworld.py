7887d2360e3c132716c63fb848ebd6dc4aa04dc1
murtazanaqvi2000@gmail.com <murtazanaqvi2000@gmail.com>
2018-05-21 22:20:53 +0530

    Basic Files

--- Helloworld.py
+++ Helloworld.py
@@ -1,1 +1,13 @@
+
+# printing a message
 print('Hello World')
+
+# Some variable declareation
+X = 18
+Fname = 'Ashwin'
+Lname = 'Semwal'
+
+# str() function to conver int to string
+print('Hello  ' + Fname + ' May God Bless You. ' + Lname + str(X))
+
+
